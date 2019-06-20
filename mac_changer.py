2b158d89c86d7d4f3cd01f3d8caf5200902fa240
mthwbrwn <mthwbrwn@gmail.com>
2019-06-20 08:45:28 -0700

    added regex to check on proper MAC change

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def change_mac(interface, new_mac):
 
@@ -17,10 +18,22 @@ def get_args():
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interface', dest='interface', help='This is the interface (eth, wlan, etc) whose MAC will be changed')
     parser.add_option('-m', '--mac', dest='new_mac', help='The new MAC address')
-    return parser.parse_args()
+    (options, args) = parser.parse_args()
+    if not options.interface:
+        parser.error('please input interface. see --help for info. ')
+    if not options.new_mac:
+        parser.error('please input new MAC value. see --help for info. ')
+    return options
 
-(options, args) = get_args()
-change_mac(options.interface, options.new_mac)
+options = get_args()
+# change_mac(options.interface, options.new_mac)
+
+check = subprocess.check_output(['ifconfig', options.interface])
+search_result = re.search(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', check)
+
+print(check)
+print(search_result.group(0))
+
 
 
 
